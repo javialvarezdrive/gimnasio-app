690188406e97b94b83129b7cc5833bb09bb6f80b
javialvarezdrive <153924249+javialvarezdrive@users.noreply.github.com>
2025-02-26 10:26:30 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
+import streamlit as st
 
 # Configuración de Supabase
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = st.secrets["SUPABASE_URL"] if "SUPABASE_URL" in st.secrets else os.getenv("SUPABASE_URL")
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"] if "SUPABASE_KEY" in st.secrets else os.getenv("SUPABASE_KEY")
 
 # Configuración de Streamlit
 APP_NAME = "Gestión de Gimnasio"
-
