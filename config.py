c3b572b11a10bb165bfe6174b6563d74de297ce3
javialvarezdrive <153924249+javialvarezdrive@users.noreply.github.com>
2025-02-26 10:04:16 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Configuración de Supabase
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+# Configuración de Streamlit
+APP_NAME = "Gestión de Gimnasio"
 
